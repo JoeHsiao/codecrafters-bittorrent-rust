dcc8a5d540fa1214735def662376bc43f4de1709
Joe Hsiao <joe.phsiao@gmail.com>
2025-10-14 16:59:12 -0500

    Parse bencoded number

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,14 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number = number_string.parse::<usize>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
         return serde_json::Value::String(string.to_string());
+    } else if let Some(num) = encoded_value.strip_prefix("i") {
+        let num = num.strip_suffix("e").expect("number has not trailing 'e'");
+        if num.contains(".") {
+            serde_json::json!(num.parse::<f64>().unwrap())
+        } else {
+            serde_json::json!(num.parse::<i64>().unwrap())
+        }
+
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
