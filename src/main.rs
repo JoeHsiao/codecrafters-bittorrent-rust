abe1ab2422249b5e21efa30f7b6661176e182263
Joe Hsiao <joe.phsiao@gmail.com>
2025-10-15 22:32:01 -0500

    Define Torrent struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,53 @@
 use serde_json;
+use serde::{Deserialize};
 use std::env;
+use serde_bencode;
 
-// Available if you need it!
-// use serde_bencode
+#[derive(Deserialize)]
+struct Torrent {
+    /// The URL of the tracker
+    announce: String,
+    info: Info,
+}
+
+#[derive(Deserialize)]
+struct Info {
+    /// In the single file case, name is the name of a file
+    /// In the multiple file case, it's the name of a directory.
+    /// It is purely advisory.
+    name: String,
+
+    /// piece length maps to the number of bytes in each piece the file is split into.
+    #[serde(rename = "piece length")]
+    piece_length: usize,
+
+    /// pieces maps to a string whose length is a multiple of 20.
+    /// It is to be subdivided into strings of length 20, each of which is the SHA1 hash of
+    /// the piece at the corresponding index.
+    pieces: Vec<u8>,
+
+    /// There is also a key length or a key files, but not both or neither.
+    /// If length is present then the download represents a single file, otherwise it represents
+    /// a set of files which go in a directory structure.
+    #[serde(flatten)]
+    files: FileList,
+}
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+#[serde(untagged)]
+enum FileList {
+    SingleFile {
+        length: usize,
+    },
+    MultiFile {
+        files: Vec<FileDetails>,
+    },
+}
+#[derive(Deserialize)]
+struct FileDetails {
+    length: usize,
+    path: Vec<String>,
+}
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
@@ -28,7 +73,7 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
                 (num.parse::<i64>().expect("Invalid number").into(), rest)
             }
         }
-        Some('0'..'9') => {
+        Some('0'..='9') => {
             let Some((len, rest)) = encoded_value.split_once(':') else {
                 panic!("String encoding does not contain ':'");
             };
